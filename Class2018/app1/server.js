0d973be936b4491e56e1621c10b91c1ee9a14e85
sengokusoul <jaysengokusoul@gmail.com>
2018-03-19 12:56:18 +0700

    fixbug

--- Class2018/app1/server.js
+++ Class2018/app1/server.js
@@ -5,7 +5,7 @@ var fs = require("fs");
 app.get('/listUsers', function (req, res) {
    fs.readFile( __dirname + "/" + "user.json", 'utf8', function (err, data) {
        console.log( data );
-       //res.end( data );
+       res.end( data );
    });
 })
 
